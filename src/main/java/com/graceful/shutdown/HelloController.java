309c82706e831579cb7e90107e85860964afcd03
APPCO\edgar.delarocha <edgar.delarocha@microsip.com>
2023-08-21 17:29:54 -0600

    Cambiando a no reactive

--- src/main/java/com/graceful/shutdown/HelloController.java
+++ src/main/java/com/graceful/shutdown/HelloController.java
@@ -2,14 +2,21 @@ package com.graceful.shutdown;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
+//import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/hola")
 public class HelloController {
 
+//  @RequestMapping("/amigo")
+//  public Mono<String>  saludar(){
+//    return Mono.just("buenos dias");
+//  }
+
   @RequestMapping("/amigo")
-  public Mono<String>  saludar(){
-    return Mono.just("buenos dias");
+  public String  saludar(){
+    return "buenos dias";
   }
+
+
 }
